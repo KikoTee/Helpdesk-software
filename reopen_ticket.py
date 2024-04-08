efedc9bca2d0b1f81e22b19f2f7928643f10ff41
Francis Timkang <161377902+KikoTee@users.noreply.github.com>
2024-04-08 12:22:02 +1200

    Update reopen_ticket.py
    trial code

--- reopen_ticket.py
+++ reopen_ticket.py
@@ -1,8 +1,11 @@
+# Function to reopen a closed ticket
 def reopen_ticket(ticket):
+    # Mark the ticket as reopened
     ticket.reopen_ticket()
+    
+    # Confirm successful ticket reopening
     print("Ticket reopened successfully!")
     print()
-
 
 # Example usage:
 ticket_to_reopen = Ticket("S101", "Bob White", "bob@example.com", "Printer not working")
